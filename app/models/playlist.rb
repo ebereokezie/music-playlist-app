29f7b030ab5b5fc3ca2faeb0842c2289596e1074
Ebere Okezie <ebereokezie@gmail.com>
2023-04-18 11:42:30 -0500

    update routes

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,4 +4,8 @@ class Playlist < ApplicationRecord
     has_many: playlist_songs
     has_many :songs, through: :playlist_songs
 
+    before_create :slugify
+    def slugify
+        self.slug = title.parameterize
+    end
 end
