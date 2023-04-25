d5ea9696fe4a80bb8ba532de75539526c3facb7f
Ebere Okezie <ebereokezie@gmail.com>
2023-04-25 12:57:12 -0500

    update song buttons to add select fuction

--- client/src/Songs.js
+++ client/src/Songs.js
@@ -15,6 +15,8 @@ function Songs() {
     const [songs, setSongs] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
     const [isLoading, setIsLoading] = useState(true);
+    const [playlists, setPlaylists] = useState([])
+    const [playlistId, setPlaylistId] = useState(null)
 
     const handleSearchTermChange = (e) =>{
         setSearchTerm(e.target.value)
@@ -35,11 +37,53 @@ function Songs() {
     })
 }, [])
 
+useEffect(() => {
+    fetch("/playlists")
+    .then((data) => {
+        if (data.ok) {
+            setIsLoading(false);
+            data.json()
+            .then(data => setPlaylists(data))
+        } else {
+            data.json()
+            .then((err) => console.log(err.errors))
+        }
+})
+}, [])
+
     const filteredSongs = songs.filter((song) =>
     song.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
     song.artist.toLowerCase().includes(searchTerm.toLowerCase()) ||
     song.album.toLowerCase().includes(searchTerm.toLowerCase())
     )
+
+    function handlePlaylistSelect(e){
+        setPlaylistId(e.target.value)
+    }
+
+    function handleAddToPlaylist(){
+        if (playlistId){
+            fetch(`/playlists/${playlistId}/playlist_songs`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ song_id: songs.id})
+                
+            })
+            .then((data) => {
+                if (data.ok) {
+                    data.json()
+                    .then((data) => setPlaylists(data))
+                }
+                else {
+                    data.json()
+                    .then((err)=> console.log(err.errors))
+                }
+            }
+            )
+        }
+    }
 
     const allSongs = filteredSongs.map((song)=> (
         <Grid item key={song.id} xs={12} sm={6} md={4}>
@@ -60,10 +104,20 @@ function Songs() {
                             {song.album}
                         </Typography>
                     </CardContent>
+                    <select value = {playlistId} onChange = {handlePlaylistSelect}>
+                        <option>Select a Playlist</option>
+                            {playlists.map((playlist) => (
+                                <option value ={playlist.id}>
+                                    {playlist.title}
+                                </option>
+                            ))}
+                    </select>
+                    <Button  sx = {{marginTop: 3, borderRadius: 3}} onClick = {handleAddToPlaylist} variant = 'contained' color = "secondary">Add to Playlist</Button>
             </Card>
         </Grid>
     ))
 
+    
     const displaySongs = filteredSongs.length !== 0 ? <Grid container spacing ={3}> {allSongs} </Grid> : <Typography >{"We don't have that song...yet!"}</Typography>
     return(
         <>
